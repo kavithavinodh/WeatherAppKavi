28310b305cfb0c1dbaf8a4801056bf2c3896b151
Kavitha <kavitha.vinodh10@icloud.com>
2021-05-07 23:02:40 +0530

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Kavitha Vinodh on 07/05/21.
+//  Copyright © 2021 Kavitha Vinodh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
